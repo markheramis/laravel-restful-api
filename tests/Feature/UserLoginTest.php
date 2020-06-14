3ac50741f3bf315d5c320e8566fa56af7a6260d2
Mark Heramis <chumheramis@gmail.com>
2020-06-14 14:15:10 +0800

    add login test
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- tests/Feature/UserLoginTest.php
+++ tests/Feature/UserLoginTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserLoginTest extends TestCase
 {
+    use WithFaker;
     /**
      * A basic feature test example.
      *
@@ -16,7 +18,82 @@ class UserLoginTest extends TestCase
     public function testExample()
     {
         $response = $this->get('/');
-
+        $test = $this->faker->firstName();
         $response->assertStatus(200);
+    }
+
+    public function testNoParameterError()
+    {
+        $response = $this->json('POST', '/api/login',[]);
+        $response
+        ->assertStatus(400)
+        ->assertJson([
+            "status" => "error",
+            "data" => "Validation Error.",
+            "message" => [
+                "email" => [
+                    "The email field is required."
+                ],
+                "password" => [
+                    "The password field is required."
+                ]
+            ]
+        ]);
+    }
+
+    public function testNoPassword()
+    {
+      $response = $this->json('POST', '/api/login',[
+          'email' => $this->faker->email(),
+      ]);
+      $response
+      ->assertStatus(400)
+      ->assertJson([
+          "status" => "error",
+          "data" => "Validation Error.",
+          "message" => [
+              "password" => [
+                  "The password field is required."
+              ]
+          ]
+      ]);
+    }
+
+    public function testWrongPassword()
+    {
+        $user = User::create($this->generateUser());
+        $response = $this->json('POST','/api/login',[
+            'email' => $user->email,
+            'password' => '1234567'
+        ]);
+        $response
+        ->assertStatus(401)
+        ->assertJson([
+            "status" => "error",
+            "message" => "Unauthorized access"
+        ]);
+    }
+
+    public function testCorrectLogin()
+    {
+        $user = User::create($this->generateUser());
+        $response = $this->json('POST','/api/login',[
+            'email' => $user->email,
+            'password' => '123456'
+        ]);
+        $response
+        ->assertStatus(200)
+        ->assertJson([
+            "status" => "success"
+        ]);
+    }
+
+    private function generateUser()
+    {
+        return [
+            'name' => $this->faker->firstName(),
+            'email' => $this->faker->email(),
+            'password' => bcrypt('123456'),
+        ];
     }
 }
