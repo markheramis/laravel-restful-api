3799c1c332ca9b2fd6bd449e524ed687c625769b
Mark Clu Heramis <chumheramis@gmail.com>
2021-08-25 06:39:33 +0800

    add destroy test for category
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ tests/Feature/CategoryDeleteTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Category;
+use Tests\Traits\userTraits;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class CategoryDeleteTest extends TestCase
+{
+    use WithFaker, userTraits;
+
+
+    public function testDestroyCategoryWithNoSessionShouldBeUnauthorized()
+    {
+        $category = Category::factory()->create();
+        $url = route("category.destroy", [$category->id]);
+        $response = $this->json("DELETE", $url);
+        $response->assertStatus(401);
+        $category->delete();
+    }
+    public function testDestroyCategoryAsAdministratorShouldBeAllowed()
+    {
+        $category = Category::factory()->create();
+        $url = route("category.destory", [$category->id]);
+        $token = $this->getTokenByRole("administrator");
+        $header = [
+            "Authorization" => "Bearer $token",
+        ];
+        $response = $this->json("DELETE", $url, [], $header);
+        $response->assertStatus(200);
+        $category->delete();
+    }
+
+    public function testDestroyCategoryAsModeratorShouldBeAllowed()
+    {
+        $category = Category::factory()->create();
+        $url = route("category.destroy", [$category->id]);
+        $token = $this->getTokenByRole("moderator");
+        $header = [
+            "Authorization" => "Bearer $token",
+        ];
+        $response = $this->json("DELETE", $url, [], $header);
+        $response->assertStatus(200);
+        $category->delete();
+    }
+
+    public function testDestroyCategoryAsSubscriberShouldBeForbidden()
+    {
+        $category = Category::factory()->create();
+        $url = route("category.destroy", [$category->id]);
+        $token = $this->getTokenByRole("subscriber");
+        $header = [
+            "Authorization" => "Bearer $token",
+        ];
+        $response = $this->json("DELETE", $url, [], $header);
+        $response->assertStatus(403);
+        $category->delete();
+    }
+}
