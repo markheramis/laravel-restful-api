d512dd7eb3d309591263ff8e7705eca1c1a21f3e
Mark Heramis <chumheramis@gmail.com>
2021-09-06 09:14:07 +0800

    fix form request authorization and rules
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- app/Http/Requests/AuthMultiFactorVerifyCodeRequest.php
+++ app/Http/Requests/AuthMultiFactorVerifyCodeRequest.php
@@ -13,7 +13,7 @@ class AuthMultiFactorVerifyCodeRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class AuthMultiFactorVerifyCodeRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'code' => ['string', 'required']
         ];
     }
 }
--- app/Http/Requests/AuthMultiFactoryGetQRCodeRequest.php
+++ app/Http/Requests/AuthMultiFactoryGetQRCodeRequest.php
@@ -13,7 +13,7 @@ class AuthMultiFactoryGetQRCodeRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,6 @@ class AuthMultiFactoryGetQRCodeRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
+        return [];
     }
 }
