4b10af180438c4a1fe0a4c086bbd9e0218b3a171
Mark Heramis <chumheramis@gmail.com>
2021-09-09 00:53:50 +0800

    add form request
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- app/Http/Controllers/API/AuthTwilio2FAController.php
+++ app/Http/Controllers/API/AuthTwilio2FAController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthTwilio2FAVerifyCodeRequest;
 
 class AuthTwilio2FAController extends Controller
 {
-    public function verifyCode(Request $request)
+    public function verifyCode(AuthTwilio2FAVerifyCodeRequest $request)
     {
         return 'verify twilio';
     }
