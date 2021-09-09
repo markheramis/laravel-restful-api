2438ab132458bfcba580fe626c8dd1932de857b2
Mark Heramis <chumheramis@gmail.com>
2021-09-10 00:31:56 +0800

    add passport configuration provider
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,5 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
