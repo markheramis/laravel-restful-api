f4596b8eba61846d92847684b8c9c5d7c52c79a7
Mark Heramis <chumheramis@gmail.com>
2021-10-07 23:55:14 +0800

    add factory to model
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Cartalyst\Sentinel\Roles\EloquentRole as Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
+    use HasFactory;
     /**
      * Get the route key for the model.
      *
