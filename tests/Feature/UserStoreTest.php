2a2d98367329a9ae2630923733d40c42298d7b64
Mark Clu Heramis <chumheramis@gmail.com>
2021-11-13 00:34:43 +0800

    update
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- tests/Feature/UserStoreTest.php
+++ tests/Feature/UserStoreTest.php
@@ -61,7 +61,7 @@ class UserStoreTest extends TestCase
     public function testStoreUserAsSubscriberShouldBeForbidden()
     {
         $user = User::factory()->make();
-        $token = $this->getTokenByRole("subscriber  ");
+        $token = $this->getTokenByRole("subscriber");
         $header = [
             "Authorization" => "Bearer $token",
         ];
@@ -72,6 +72,6 @@ class UserStoreTest extends TestCase
             "first_name" => $user->first_name,
             "last_name" => $user->last_name,
         ], $header);
-        $response->assertStatus(200);
+        $response->assertStatus(403);
     }
 }
