f97685697824447c24f4b45a2c1fe22b7c4e9df8
Mark Heramis <chumheramis@gmail.com>
2021-11-15 04:50:31 +0800

    update tests
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- tests/Feature/UserStoreTest.php
+++ tests/Feature/UserStoreTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Sentinel;
 use Tests\TestCase;
 use App\Models\User;
 use Tests\Traits\userTraits;
@@ -24,10 +25,10 @@ class UserStoreTest extends TestCase
         $response->assertStatus(401);
     }
 
-    public function testStoreUserAsAdministratorShouldBeAllowed()
+    public function testStoreUserAsSubscriberShouldBeForbidden()
     {
         $user = User::factory()->make();
-        $token = $this->getTokenByRole("administrator");
+        $token = $this->getTokenByRole("subscriber");
         $header = [
             "Authorization" => "Bearer $token",
         ];
@@ -38,7 +39,7 @@ class UserStoreTest extends TestCase
             "first_name" => $user->first_name,
             "last_name" => $user->last_name,
         ], $header);
-        $response->assertStatus(200);
+        $response->assertStatus(403);
     }
 
     public function testStoreUserAsModeratorShouldBeForbidden()
@@ -58,10 +59,21 @@ class UserStoreTest extends TestCase
         $response->assertStatus(403);
     }
 
-    public function testStoreUserAsSubscriberShouldBeForbidden()
+    public function testStoreUserAsAdministratorWithMfaNotVerifiedShouldBeForbidden()
     {
+        $test_user = json_decode(config("authy.test_user"));
+        $session_user = User::factory([
+            'country_code' => $test_user->country_code,
+            'phone_number' => $test_user->phone_number,
+            'authy_id' => $test_user->authy_id,
+            'authy_enabled' => 1,
+            'default_factor' => 'sms',
+
+        ])->create();
+        $selectedRole = Sentinel::findRoleBySlug("administrator");
+        $selectedRole->users()->attach($session_user);
         $user = User::factory()->make();
-        $token = $this->getTokenByRole("subscriber");
+        $token = $this->getTokenByRole("administrator", $session_user->id, false);
         $header = [
             "Authorization" => "Bearer $token",
         ];
@@ -72,6 +84,36 @@ class UserStoreTest extends TestCase
             "first_name" => $user->first_name,
             "last_name" => $user->last_name,
         ], $header);
-        $response->assertStatus(403);
+        $response->assertStatus(401);
+        $session_user->delete();
+    }
+
+    public function testStoreUserAsAdministratorWithMfaVerifiedShouldBeAllowed()
+    {
+        $test_user = json_decode(config("authy.test_user"));
+        $session_user = User::factory([
+            'country_code' => $test_user->country_code,
+            'phone_number' => $test_user->phone_number,
+            'authy_id' => $test_user->authy_id,
+            'authy_enabled' => 1,
+            'default_factor' => 'sms',
+
+        ])->create();
+        $selectedRole = Sentinel::findRoleBySlug("administrator");
+        $selectedRole->users()->attach($session_user);
+        $user = User::factory()->make();
+        $token = $this->getTokenByRole("administrator", $session_user->id, true);
+        \Log::info("MFA Token: $token");
+        $header = [
+            "Authorization" => "Bearer $token",
+        ];
+        $response = $this->json("POST", route("user.store"), [
+            "username" => $user->username,
+            "email" => $user->email,
+            "password" => "p@s5W0rd12345",
+            "first_name" => $user->first_name,
+            "last_name" => $user->last_name,
+        ], $header);
+        $response->assertStatus(200);
     }
 }
