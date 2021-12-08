ea51d26600b4ea1663f8333bed92bfc201234c30
Carlomar Espinosa <carlomar@wendyourway.com>
2021-12-09 01:12:52 +0800

    changed all test case no mfa 404 to 403

--- tests/Feature/UserStoreTest.php
+++ tests/Feature/UserStoreTest.php
@@ -108,7 +108,7 @@ class UserStoreTest extends TestCase
             "first_name" => $user->first_name,
             "last_name" => $user->last_name,
         ], $header);
-        $response->assertStatus(401);
+        $response->assertStatus(403);
         $session_user->delete();
     }
 
