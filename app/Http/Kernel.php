b5db7077355d0b5a4ad5ca91baa2b6bb561b613f
Mark Clu Heramis <chumheramis@gmail.com>
2023-03-06 23:00:22 +0800

    remove php cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
